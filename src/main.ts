6d9e7f438b5c7e12fec50a68f008564e39fdc166
Sreekanth D <inbox.sreekanthd@gmail.com>
2025-01-14 21:19:07 +0530

    feat: enhance content and improve image loading in gallery

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 window.addEventListener("load", () => {
   displayAfterLoad();
-  const images = Array.from({ length: 6 }, (_, i) => ({
+  const images = Array.from({ length: 10 }, (_, i) => ({
     src: getImage(i + 1),
     alt: `Image ${i + 1}`,
     caption: `Image ${i + 1}`,
